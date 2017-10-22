9b8d35c2f31a5628a7a12aa6d1ab8b6bd19e5181
Thorben Jensen <jensen.thorben@gmail.com>
2017-10-22 13:21:16 +0200

    added big part of preprocessing to text classification script.

--- muenster_events/classify.py
+++ muenster_events/classify.py
@@ -1,6 +1,33 @@
 """Classify events to be relevant for families, or not."""
 
-# events_df[['id', 'title', 'subtitle', 'details']]
-# events_df['text'] = events_df['title'] \
-#     + ' - ' + events_df['subtitle'] \
-#     + ' - ' + events_df['details']
+import model
+import re
+import nltk
+from nltk.corpus import stopwords
+
+# load data
+df = model.read_events_df()
+df = df.dropna()
+
+# PREPROCESSING
+# all text in one column
+df['text'] = df['title'] \
+    + ' - ' + df['subtitle'] \
+    + ' - ' + df['details']
+df = df[['id', 'text', 'label_family']]
+# process text
+df['text'] = df['text'].map(lambda x: re.sub(r'\W+', ' ', x))
+df['text'] = df['text'].str.lower()
+# remove stopwords
+stop = stopwords.words('german')
+df['text'] = df['text'].str.split()
+df['text'] = df['text'].apply(lambda x: [i for i in x if i not in stop])
+
+# extract feature matrix and label vector
+# TODO
+
+# train model
+# TODO
+
+# evaluate metrics
+# TODO
