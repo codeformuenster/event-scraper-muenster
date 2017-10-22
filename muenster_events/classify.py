3c1fb51423b8f97f4f26468b17187e33a1e31e05
Thorben Jensen <jensen.thorben@gmail.com>
2017-10-23 00:20:00 +0200

    finished naive bayesian classifier

--- muenster_events/classify.py
+++ muenster_events/classify.py
@@ -2,8 +2,12 @@
 
 import model
 import re
-import nltk
+import pandas as pd
 from nltk.corpus import stopwords
+from sklearn.naive_bayes import MultinomialNB
+from sklearn import metrics
+from sklearn import model_selection
+from nltk.stem.snowball import GermanStemmer
 
 # load data
 df = model.read_events_df()
@@ -18,16 +22,50 @@ df = df[['id', 'text', 'label_family']]
 # process text
 df['text'] = df['text'].map(lambda x: re.sub(r'\W+', ' ', x))
 df['text'] = df['text'].str.lower()
-# remove stopwords
+# remove stopwords and stem
 stop = stopwords.words('german')
+gs = GermanStemmer()
 df['text'] = df['text'].str.split()
-df['text'] = df['text'].apply(lambda x: [i for i in x if i not in stop])
+df['text'] = df['text'].apply(lambda x:
+                              [gs.stem(i) for i in x if i not in stop])
 
 # extract feature matrix and label vector
-# TODO
+X = df['text'].str.join('|').str.get_dummies().add_prefix('word_')
+y = df['label_family']
 
-# train model
-# TODO
+# test ft-idf
+# v = TfidfVectorizer()
+# X = pd.DataFrame(data=v.fit_transform(df['text']).toarray(),
+#              columns=v.get_feature_names())
 
-# evaluate metrics
-# TODO
+# define models
+mnb = MultinomialNB()
+
+# evaluate models
+folds = y.loc[y == 1.0].size
+cv = model_selection.cross_val_score(mnb, X, y, cv=folds, scoring='f1')
+print('F1 score from CV: ')
+print(cv.mean())
+precision = model_selection.cross_val_score(mnb, X, y, cv=folds,
+                                            scoring='precision')
+print('Precision from CV: ')
+print(precision.mean())
+recall = model_selection.cross_val_score(mnb, X, y, cv=folds, scoring='recall')
+print('Recall scores from CV: ')
+print(recall.mean())
+
+# predict on training data
+fit = mnb.fit(X, y)
+y_predict = fit.predict(X)
+cfm = metrics.confusion_matrix(y, y_predict, labels=[1, 0])
+print('confusion matrix (on training data):')
+print(cfm)
+f1 = metrics.f1_score(y, y_predict)
+print('f1 score (on training data):')
+print(f1)
+
+# inspect probabilities per feature
+df_weights = pd.DataFrame(data=fit.coef_, columns=X.columns).transpose()
+df_weights.columns = ['score']
+
+print(df_weights.sort_values('score'))
